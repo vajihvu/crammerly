747b5172ba2b045a6676670c1a7bc339daaf1908
vajihvu <vajih.101@gmail.com>
2026-04-12 15:45:10 +0530

    fix: corrected asyncHandler import path in adminController

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,5 +1,5 @@
 import User from '../models/User.js';
-import { asyncHandler } from '../middleware/asyncHandler.js';
+import asyncHandler from '../utils/asyncHandler.js';
 import { logger } from '../utils/logger.js';
 
 /**
